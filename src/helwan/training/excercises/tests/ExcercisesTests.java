42cc0381b1bf217ce4544ff2503942146811b3ee
KhaledRoushdy <khaled_roushdy13@hotmail.com>
2017-02-10 23:12:16 +0200

    Add new excercises

--- src/helwan/training/excercises/tests/ExcercisesTests.java
+++ src/helwan/training/excercises/tests/ExcercisesTests.java
@@ -1,17 +1,32 @@
 package helwan.training.excercises.tests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExcercisesTests {
 
+	WebDriver driver;
+	
+	@Before
+	public void setup(){
+	
+		System.setProperty("webdriver.chrome.driver", "./ChromeDriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
 	@Test
 	public void excercise_1() {
 		
-		System.setProperty("webdriver.chrome.driver", "./ChromeDriver/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.store.demoqa.com");
 		String pageTitle = driver.getTitle();   //get the title 
 		String pageUrl = driver.getCurrentUrl(); //get the currect url
@@ -29,6 +44,43 @@ public class ExcercisesTests {
 		Assert.assertNotNull("The page title is not correct", pageTitle);
 		Assert.assertNotEquals("The page source is not correct", 0, pageSourceLength);
 		driver.navigate().refresh(); //refresh the page
-		driver.close();  //close the driver
+		 
 	}
-}
+	
+	@Test
+	public void selectExcerise(){
+			
+			driver.get("http://toolsqa.com/automation-practice-form/");
+			Select select=new Select(driver.findElement(By.id("selenium_commands")));
+			select.selectByVisibleText("Browser Commands");
+			select.deselectByIndex(0);
+			
+			for(WebElement option:select.getOptions()){
+				
+				System.out.println(option.getText());
+				option.click();
+			}
+			
+			select.deselectAll();
+		}
+	
+	@Test
+	public void waitExcercise(){
+		
+		driver.get("http://google.com");
+		driver.findElement(By.id("lst-ib")).sendKeys("itworx");
+		driver.findElement(By.name("btnK")).click();
+		WebDriverWait wait=new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"hdtb-msb\"]/div[1]/div[2]/a")));
+		driver.findElement(By.xpath("//*[@id=\"hdtb-msb\"]/div[1]/div[2]/a")).click();
+		
+	}
+	
+	
+	@After
+	public void tearDown(){
+		driver.close();
+	}
+	
+	}
+
